b255ca4861358c6d76361ad2ec56a00292dbaf2e
HelloPain <1738435031@qq.com>
2023-11-11 16:38:21 +0800

    dwd cancel order implemented

--- src/main/java/app/dwd/DwdCancelOrderDetail.java
+++ src/main/java/app/dwd/DwdCancelOrderDetail.java
@@ -2,22 +2,21 @@ package app.dwd;
 
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.Table;
-import org.apache.flink.table.api.TableConfig;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 import util.Common;
 import util.FlinkSqlUtil;
 
-import java.time.Duration;
+
 
 /**
  * @Author: PJ, SATAN LOVES YOU FOREVER
  * @Date: 2023/11/7 10:46
  * @Function: Read topic_db from kafka,
- *            extract `order_info`,`order_detail`,`order_detail_activity`,`order_detail_coupon` data from topic_db,
- *            sink to kafka
- * @DataLink: mock -> maxwell -> kafka(topic_db) -> flink table -> kafka(dwd_trade_order_detail)
+ * extract `order_info`,`order_detail`,`order_detail_activity`,`order_detail_coupon` data from topic_db,
+ * sink to kafka
+ * @DataLink: mock -> maxwell -> kafka(topic_db) -> flink table -> kafka(dwd_trade_cancel_detail)
  */
-public class DwdTradeOrderDetail {
+public class DwdCancelOrderDetail {
     public static void main(String[] args) throws Exception {
         //1.read from kafka topic_db, turn it into flink table
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
@@ -25,45 +24,47 @@ public class DwdTradeOrderDetail {
         StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
         tableEnv.executeSql(FlinkSqlUtil.createTopicDBFlinkTable());
         //tableEnv.sqlQuery("select * from ods_topic_db").execute().print();
-        //设置TTL
-        TableConfig config = tableEnv.getConfig();
-        //4张表的数据同时生成,给定的时间只需要考虑生产延迟即可
-        config.setIdleStateRetention(Duration.ofSeconds(10));
 
-        //2.get order info tables
-        Table orderInfo = tableEnv.sqlQuery("select\n" +
-                "    `data`['id'] id,  \n" +
-                "    `data`['consignee'] consignee,  \n" +
-                "    `data`['consignee_tel'] consignee_tel,  \n" +
-                "    `data`['total_amount'] total_amount,  \n" +
-                "    `data`['order_status'] order_status,  \n" +
-                "    `data`['user_id'] user_id,  \n" +
-                "    `data`['payment_way'] payment_way,  \n" +
-                "    `data`['delivery_address'] delivery_address,  \n" +
-                "    `data`['order_comment'] order_comment,  \n" +
-                "    `data`['out_trade_no'] out_trade_no,  \n" +
-                "    `data`['trade_body'] trade_body,  \n" +
-                "    `data`['create_time'] create_time,  \n" +
-                "    `data`['operate_time'] operate_time,  \n" +
-                "    `data`['expire_time'] expire_time,  \n" +
-                "    `data`['process_status'] process_status,  \n" +
-                "    `data`['tracking_no'] tracking_no,  \n" +
-                "    `data`['parent_order_id'] parent_order_id,  \n" +
-                "    `data`['province_id'] province_id,  \n" +
-                "    `data`['activity_reduce_amount'] activity_reduce_amount,  \n" +
-                "    `data`['coupon_reduce_amount'] coupon_reduce_amount,  \n" +
-                "    `data`['original_total_amount'] original_total_amount,  \n" +
-                "    `data`['feight_fee'] feight_fee,  \n" +
-                "    `data`['feight_fee_reduce'] feight_fee_reduce,  \n" +
-                "    `data`['refundable_time'] refundable_time\n" +
-                "from ods_topic_db\n" +
-                "where `database` = 'gmall_flink'\n" +
-                "      and `table` = 'order_info'\n" +
-                "      and `type` = 'insert';");
-        tableEnv.createTemporaryView("order_info", orderInfo);
-        //tableEnv.sqlQuery("select * from order_info").execute().print();
+        //set TTL for join
+        //TableConfig config = tableEnv.getConfig();
+        //config.setIdleStateRetention(Duration.ofSeconds(10));
 
-        Table orderDetail = tableEnv.sqlQuery("select\n" +
+        //2.get canceled order info from topic_db
+        Table orderInfo = tableEnv.sqlQuery(
+                   "select\n" +
+                   "    `data`['id'] id,  \n" +
+                   "    `data`['consignee'] consignee,  \n" +
+                   "    `data`['consignee_tel'] consignee_tel,  \n" +
+                   "    `data`['total_amount'] total_amount,  \n" +
+                   "    `data`['order_status'] order_status,  \n" +
+                   "    `data`['user_id'] user_id,  \n" +
+                   "    `data`['payment_way'] payment_way,  \n" +
+                   "    `data`['delivery_address'] delivery_address,  \n" +
+                   "    `data`['order_comment'] order_comment,  \n" +
+                   "    `data`['out_trade_no'] out_trade_no,  \n" +
+                   "    `data`['trade_body'] trade_body,  \n" +
+                   "    `data`['create_time'] create_time,  \n" +
+                   "    `data`['operate_time'] operate_time,  \n" +
+                   "    `data`['expire_time'] expire_time,  \n" +
+                   "    `data`['process_status'] process_status,  \n" +
+                   "    `data`['tracking_no'] tracking_no,  \n" +
+                   "    `data`['parent_order_id'] parent_order_id,  \n" +
+                   "    `data`['province_id'] province_id,  \n" +
+                   "    `data`['activity_reduce_amount'] activity_reduce_amount,  \n" +
+                   "    `data`['coupon_reduce_amount'] coupon_reduce_amount,  \n" +
+                   "    `data`['original_total_amount'] original_total_amount,  \n" +
+                   "    `data`['refundable_time'] refundable_time\n" +
+                   "from ods_topic_db\n" +
+                   "where `database` = 'gmall_flink'\n" +
+                   "      and `table` = 'order_info'\n" +
+                   "      and `type` = 'update'\n" +
+                   "      and `old`['order_status'] is not null\n" +
+                   "      and `data`['order_status'] = '1003';");//TODO: better look up join dim_base_province
+        tableEnv.createTemporaryView("order_cancel_info", orderInfo);
+        //tableEnv.sqlQuery("select * from order_cancel_info").execute().print();
+
+        Table orderDetail = tableEnv.sqlQuery(
+                "select\n" +
                 "    `data`['id'] id,\n" +
                 "    `data`['order_id'] order_id,\n" +
                 "    `data`['sku_id'] sku_id,\n" +
@@ -85,7 +86,8 @@ public class DwdTradeOrderDetail {
         tableEnv.createTemporaryView("order_detail", orderDetail);
         //tableEnv.sqlQuery("select * from order_detail").execute().print();
 
-        Table orderDetailActivity = tableEnv.sqlQuery("select\n" +
+        Table orderDetailActivity = tableEnv.sqlQuery(
+                "select\n" +
                 "  `data`['id'] id,\n" +
                 "  `data`['order_id'] order_id,\n" +
                 "  `data`['order_detail_id'] order_detail_id,\n" +
@@ -101,7 +103,8 @@ public class DwdTradeOrderDetail {
         tableEnv.createTemporaryView("order_detail_activity", orderDetailActivity);
         //tableEnv.sqlQuery("select * from order_detail_activity").execute().print();
 
-        Table orderDetailCoupon = tableEnv.sqlQuery("select\n" +
+        Table orderDetailCoupon = tableEnv.sqlQuery(
+                "select\n" +
                 "   `data`['id'] id,\n" +
                 "   `data`['order_id'] order_id,\n" +
                 "   `data`['order_detail_id'] order_detail_id,\n" +
@@ -118,7 +121,8 @@ public class DwdTradeOrderDetail {
         //tableEnv.sqlQuery("select * from order_detail_coupon").execute().print();
 
         //3.join
-        Table joined = tableEnv.sqlQuery("select\n" +
+        Table joined = tableEnv.sqlQuery(
+                "select\n" +
                 "      od.id order_detail_id,\n" +
                 "      oi.id order_id,\n" +
                 "      oi.consignee,\n" +
@@ -139,8 +143,6 @@ public class DwdTradeOrderDetail {
                 "      oi.activity_reduce_amount,\n" +
                 "      oi.coupon_reduce_amount,\n" +
                 "      oi.original_total_amount,\n" +
-                "      oi.feight_fee,\n" +
-                "      oi.feight_fee_reduce,\n" +
                 "      oi.refundable_time,\n" +
                 "\n" +
                 "      od.sku_id,\n" +
@@ -163,14 +165,15 @@ public class DwdTradeOrderDetail {
                 "      oc.coupon_use_id\n" +
                 "\n" +
                 "from \n" +
-                "      order_info oi join order_detail od on oi.id = od.order_id\n" +
+                "      order_cancel_info oi join order_detail od on oi.id = od.order_id\n" +
                 "      left join order_detail_activity oa on od.id = oa.order_detail_id\n" +
                 "      left join order_detail_coupon oc on od.id = oc.order_detail_id");
         tableEnv.createTemporaryView("joined", joined);
         //tableEnv.sqlQuery("select * from joined").execute().print();
 
         //4.sink to kafka
-        tableEnv.executeSql("create table dwd_order_add(\n" +
+        tableEnv.executeSql(
+                "create table dwd_cancel_order_detail(\n" +
                 "      order_detail_id string,\n" +
                 "      order_id string,\n" +
                 "      consignee string,\n" +
@@ -191,8 +194,6 @@ public class DwdTradeOrderDetail {
                 "      activity_reduce_amount string,\n" +
                 "      coupon_reduce_amount string,\n" +
                 "      original_total_amount string,\n" +
-                "      feight_fee string,\n" +
-                "      feight_fee_reduce string,\n" +
                 "      refundable_time string,\n" +
                 "      sku_id string,\n" +
                 "      sku_name string,\n" +
@@ -211,9 +212,9 @@ public class DwdTradeOrderDetail {
                 "      coupon_id string,\n" +
                 "      coupon_use_id string,\n" +
                 "    PRIMARY KEY (`order_detail_id`) NOT ENFORCED\n" +
-                ")"+ FlinkSqlUtil.getUpsertKafkaProducerDDL(Common.TOPIC_DWD_TRADE_ORDER_DETAIL));
+                ")" + FlinkSqlUtil.getUpsertKafkaProducerDDL(Common.TOPIC_DWD_TRADE_CANCEL_DETAIL));
 
-        tableEnv.executeSql("insert into dwd_order_add select * from joined");
+        tableEnv.executeSql("insert into dwd_cancel_order_detail select * from joined");
 
         //env.execute();
     }
